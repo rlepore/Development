7291cf65c087a4007477e2708ac54c4e0d5adfd8
Ralph Lepore <ralphlepore@gmail.com>
2013-06-29 00:31:26 -0400

    Added global configuration

---
+++ dotfiles/emacs.d/ralph/global.el
@@ -0,0 +1,9 @@
+;; Display line and column numbers
+(line-number-mode 1) 
+(column-number-mode 1)
+
+(desktop-save-mode 1)
+
+;; Disable control z
+(global-unset-key "\C-z")
+(global-unset-key "\C-x\C-z")
